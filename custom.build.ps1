6339ecc2407d7e3b7d671acf419312ac90905cc8
codaamok <me@cookadam.co.uk>
2022-09-18 18:31:24 +0100

    Updated custom.build.ps1 +semver: skip

--- custom.build.ps1
+++ custom.build.ps1
@@ -24,11 +24,51 @@ param (
 )
 
 task PreBuild {
+    if (-not (Get-Module 'ConfigurationManager' -ListAvailable)) {
+        Invoke-BuildClean -Path $BuildRoot\misc
+        New-Item -Path $BuildRoot\misc\ConfigurationManager\ConfigurationManager.psm1 -ItemType File -Force
+        $Params = @{
+            Path = '{0}\misc\ConfigurationManager\ConfigurationManager.psd1' -f $BuildRoot
+            Guid = (New-Guid).Guid
+            RootModule = '{0}\misc\ConfigurationManager\ConfigurationManager.psm1' -f $BuildRoot
+        }
+        New-ModuleManifest @Params
 
+        $SuccessfullyCopiedModule = $false
+        foreach ($ModulePath in $env:PSModulePath.Split([System.IO.Path]::PathSeparator)) {
+            try {
+                $Params = @{
+                    Path        = '{0}\misc\ConfigurationManager' -f $BuildRoot
+                    Destination = $ModulePath
+                    Recurse     = $true
+                    Force       = $true
+                    PassThru    = $true
+                    ErrorAction = 'Stop'
+                }
+                Copy-Item @Params
+                $SuccessfullyCopiedModule = $true
+                break
+            }
+            catch {
+                Write-Warning -Message ('Failed to copy dummy ConfigurationManager module to "{0}" ({1})' -f $ModuleName, $_.Exception.Message)
+            }
+        }
+
+        if (-not $SuccessfullyCopiedModule) {
+            Write-Error "Failed to copy dummy ConfigurationManager module to a PSModulePath folder" -ErrorAction "Stop"
+        }
+    }
 }
 
 task PostBuild {
-
+    Invoke-BuildClean -Path $BuildRoot\misc
+    if ($env:USER -ne 'runner') {
+        Get-Module 'ConfigurationManager' -ListAvailable | ForEach-Object {
+            if ($_ -notmatch 'AdminConsole\\bin') {
+                Remove-Item -Path $_.ModuleBase -Recurse -Force -ErrorAction 'Stop'
+            }
+        }
+    }
 }
 
 task PreRelease {
@@ -36,5 +76,9 @@ task PreRelease {
 }
 
 task PostRelease {
-
+    Get-Module 'ConfigurationManager' -ListAvailable | ForEach-Object {
+        if ($_ -notmatch 'AdminConsole\\bin') {
+            Remove-Item -Path $_.ModuleBase -Recurse -Force -ErrorAction 'Stop'
+        }
+    }
 }
